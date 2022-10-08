fbf9b7b33f9cd5f480845375a54f9f50bca22481
Amy Woodworth <woodworthcommunications@gmail.com>
2022-10-08 18:54:05 -0500

    Change input type in form. Update CSS. Start on bill input validation.

--- app.js
+++ app.js
@@ -23,6 +23,31 @@ function formatInputTotal(number) {
   return Number(number.value);
 }
 
+function showBillInputErrorMessage() {
+  const billErrorMessage = document.querySelector('.bill__total .form__error-message');
+  const dollarAmount = billAmount.value;
+  const resetAmount = billAmount.value = '';
+
+  // Dollar value can't be 0 
+  if ( dollarAmount <= 0 ) {
+    billErrorMessage.classList.remove('not-visible');
+    billAmount.classList.add('invalid');
+    billAmount.setAttribute('aria-invalid', true);
+    resetAmount;
+
+    console.log('error!');
+  // Dollar value can't be string  
+  } else if ( dollarAmount === NaN ) {
+    // resetAmountValue;
+    console.log('error!');
+  // Dollar value can't be empty string
+  } else if ( dollarAmount === '' ) {
+    console.log('error!');
+  } else {
+    console.log(formatInputTotal(bill));
+  }
+}
+
 // Enable button so form can be reset form
 [billAmount, numberOfPeople].forEach(inputField => {
   inputField.addEventListener('click', () => {
@@ -33,6 +58,7 @@ function formatInputTotal(number) {
 // Format billAmount so only 2 decimal places displayed after input entered
 billAmount.addEventListener('change', e => {
   e.currentTarget.value = parseFloat(e.currentTarget.value).toFixed(2);
+  // console.log(billAmount.checkValidity());
 });
 
 // Get tip % from radio button
@@ -48,10 +74,10 @@ customTip.addEventListener('click', () => {
   // Start custom tip at 0 percent
   customTip.value = 0;
 
-  // if (radioBtnTips.value) {
-  //   uncheckRadioBtn();
-  //   tipPercentage = customTip.value;
-  // }
+  if (radioBtnTips.value) {
+    uncheckRadioBtn();
+    tipPercentage = customTip.value;
+  }
 });
 
 // Reset form
